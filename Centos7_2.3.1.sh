2bc5328074cd100402ccace4392d4b64aa4237a6
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-01-07 12:28:28 +0530

    Add files via upload

---
+++ Centos7_2.3.1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 2.3.1  Ensure NIS Client is not installed
+echo
+echo \*\*\*\* 2.3.1 Ensure NIS Client is not installed
+rpm -q ypbind && yum -y remove ypbind
+
+
+
+
+
---
+++ Centos7_2.3.5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 2.3.5 Ensure LDAP client is not installed
+echo
+echo \*\*\*\* 2.3.5 Ensure LDAP client is not installed
+rpm -q openldap-clients && yum -y remove openldap-clients
+
+
+
+
+
