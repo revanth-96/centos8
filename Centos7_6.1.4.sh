11bdff5bb9290edc4be65d5fa27c3515bb71dd6d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-01-07 12:57:56 +0530

    Add files via upload

---
+++ Centos7_6.1.4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 6.1.4 Ensure permissions on /etc/shadow are configured
+echo
+echo \*\*\*\* 6.1.4 Ensure permissions on /etc/shadow are configured
+chmod -t,u-x-s,g-w-x-s,o-r-w-x /etc/shadow
---
+++ Centos7_6.1.7.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 6.1.7 Ensure permissions on /etc/gshadow are configured
+echo
+echo \*\*\*\* 6.1.7 Ensure permissions on /etc/gshadow are configured
+chmod -t,u-x-s,g-w-x-s,o-r-w-x /etc/gshadow
