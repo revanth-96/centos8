d2dd559fd639100a8c9cee7dd317024b50b06565
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-07 17:37:31 +0530

    Update and rename Centos7_5.3.4.sh to Centos8_5.2.2.sh

--- Centos8_5.2.2.sh
+++ Centos8_5.2.2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 5.3.4 Ensure SSH access is limited
+# 5.2.2 Ensure SSH access is limited
 #echo
-#echo \*\*\*\* 5.3.4 Ensure SSH access is limited
+#echo \*\*\*\* 5.2.2 Ensure SSH access is limited
 #grep "^AllowUsers" /etc/ssh/sshd_config || echo "allowusers <userlist>" >> /etc/ssh/sshd_config
 #grep "^AllowGroups" /etc/ssh/sshd_config || echo "allowgroups <grouplist>" >> /etc/ssh/sshd_config
 #grep "^DenyUsers" /etc/ssh/sshd_config || echo "denyusers <userlist>" >> /etc/ssh/sshd_config
