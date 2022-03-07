0bd0caf889cd9d4efb9a6b22edf6d0fe7c4ac922
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-07 11:57:42 +0530

    Update and rename Centos7_1.1.1.3.sh to Centos8_1.1.1.4.sh

--- Centos8_1.1.1.4.sh
+++ Centos8_1.1.1.4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 1.1.1.3 Ensure mounting of udf filesystems is disabled
+# 1.1.1.4 Ensure mounting of udf filesystems is disabled
 echo
-echo \*\*\*\* 1.1.1.3 Ensure mounting of udf filesystems is disabled
+echo \*\*\*\* 1.1.1.4 Ensure mounting of udf filesystems is disabled
 modprobe -n -v udf | grep "install /bin/true" || echo "install udf /bin/true" >> /etc/modprobe.d/CIS.conf
 lsmod | egrep "udf" && rmmod udf
 
