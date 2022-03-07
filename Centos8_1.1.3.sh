5392f6ca7deadbc29ac02351e565a688ba818ff2
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-07 12:00:24 +0530

    Update and rename Centos7_1.1.4.sh to Centos8_1.1.3.sh

---
+++ Centos8_1.1.3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 1.1.3 Ensure nodev option set on /tmp partition
+echo
+echo \*\*\*\* 1.1.3 Ensure nodev option set on /tmp partition
+findmnt -n /tmp | grep -i nodev || mount -o remount,nosuid,nodev,noexec /tmp
+
+
+
