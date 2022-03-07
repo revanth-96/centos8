746d57c0b8a2e6d73a3b75954e4cda5decef8b35
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-07 11:56:53 +0530

    Update and rename Centos7_1.1.1.2.sh to Centos8_1.1.1.3.sh

--- Centos8_1.1.1.3.sh
+++ Centos8_1.1.1.3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# 1.1.1.2  Ensure mounting of squashfs filesystems is disabled
+# 1.1.1.3  Ensure mounting of squashfs filesystems is disabled
 echo
-echo \*\*\*\* 1.1.1.2 Ensure mounting of squashfs filesystems is disabled
+echo \*\*\*\* 1.1.1.3 Ensure mounting of squashfs filesystems is disabled
 modprobe -n -v squashfs | grep "install /bin/true" || echo "install squashfs /bin/true" >> /etc/modprobe.d/CIS.conf
 lsmod | egrep "squashfs" && rmmod squashfs
 
