6dae035ad26b00428223379ce07b4f68a545ba61
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-07 17:30:57 +0530

    Delete Centos7_5.3.13.sh

--- Centos7_5.3.13.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 5.3.13 Ensure only strong Ciphers are used
-echo
-echo \*\*\*\* 5.3.13 Ensure only strong Ciphers are used
-echo "Ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com" >> /etc/ssh/sshd_config
