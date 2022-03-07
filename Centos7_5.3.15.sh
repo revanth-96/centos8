fea23e713bdcb1f5156a8fe8f397d4e953ebee05
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-07 17:32:19 +0530

    Delete Centos7_5.3.15.sh

--- Centos7_5.3.15.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# 5.3.15 Ensure only strong Key Exchange algorithms are used
-echo
-echo \*\*\*\* 5.3.15 Ensure only strong Key Exchange algorithms are used
-grep -i "KexAlgorithms" /etc/ssh/sshd_config || echo "KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256" >> /etc/ssh/sshd_config
