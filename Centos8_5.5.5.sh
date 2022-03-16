fd7a6a6dd77f492234c3958e894a47f40cbbdd57
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-16 13:13:00 +0530

    Update Centos8_5.5.5.sh

--- Centos8_5.5.5.sh
+++ Centos8_5.5.5.sh
@@ -5,3 +5,4 @@ echo
 echo \*\*\*\* 5.5.5 Ensure default user umask is 027 or more restrictive
 egrep -q "^(\s*)umask\s+\S+(\s*#.*)?\s*$" /etc/bashrc && sed -ri "s/^(\s*)umask\s+\S+(\s*#.*)?\s*$/\1umask 027\2/" /etc/bashrc || echo "umask 027" >> /etc/bashrc
 egrep -q "^(\s*)umask\s+\S+(\s*#.*)?\s*$" /etc/profile && sed -ri "s/^(\s*)umask\s+\S+(\s*#.*)?\s*$/\1umask 027\2/" /etc/profile || echo "umask 027" >> /etc/profile
+egrep -q "^(\s*)umask\s+\S+(\s*#.*)?\s*$" /etc/profile && sed -ri "s/^(\s*)umask\s+\S+(\s*#.*)?\s*$/\1umask 027\2/" /etc/profile || echo "umask 027" | tee -a /etc/rsyslog.conf /etc/profile.d/*.sh > /dev/null
