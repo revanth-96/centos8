77c4239cb61f80e78d3bf347ff1783f440dbedd9
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-16 13:11:48 +0530

    Update Centos8_2.2.3.sh

--- Centos8_2.2.3.sh
+++ Centos8_2.2.3.sh
@@ -3,8 +3,8 @@
 # 2.2.3 Ensure rsync service is not enabled
 echo
 echo \*\*\*\* 2.2.3 Ensure rsync service is not enabled
-systemctl --now mask rsyncd
-
+#systemctl --now mask rsyncd
+systemctl --now disable rsyncd
 
 
 
