601713bdad84a0b0a5e0c28fdc1c2cc903920223
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-16 13:10:53 +0530

    Update Centos8_2.2.1.2.sh

--- Centos8_2.2.1.2.sh
+++ Centos8_2.2.1.2.sh
@@ -3,8 +3,8 @@
 # 2.2.1.2 Ensure chrony is configured
 echo
 echo \*\*\*\* 2.2.1.2 Ensure chrony is configured
-grep ^OPTIONS /etc/sysconfig/chronyd && sed -i "/^OPTIONS=""/ cOPTIONS=\"-u chrony\"" /etc/sysconfig/chronyd || echo "OPTIONS="-u chrony"" >> /etc/sysconfig/chronyd
-
+#grep ^OPTIONS /etc/sysconfig/chronyd && sed -i "/^OPTIONS=""/ cOPTIONS=\"-u chrony\"" /etc/sysconfig/chronyd || echo "OPTIONS="-u chrony"" >> /etc/sysconfig/chronyd
+grep -E ^server /etc/chrony.conf || echo "server 10.0.0.2" >> /etc/chrony.conf
 
 
 
