3777168a1b41a9ea93725f7787ce7d0abe3a1340
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-21 12:55:41 +0530

    Update Centos8_2.2.1.2.sh

--- Centos8_2.2.1.2.sh
+++ Centos8_2.2.1.2.sh
@@ -5,6 +5,8 @@ echo
 echo \*\*\*\* 2.2.1.2 Ensure chrony is configured
 #grep ^OPTIONS /etc/sysconfig/chronyd && sed -i "/^OPTIONS=""/ cOPTIONS=\"-u chrony\"" /etc/sysconfig/chronyd || echo "OPTIONS="-u chrony"" >> /etc/sysconfig/chronyd
 grep -E ^server /etc/chrony.conf || echo "server 10.0.0.2" >> /etc/chrony.conf
+#start chrony if not started
+systemctl start chronyd 
 
 
 
