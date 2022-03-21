e541943cb61df46fe6b9ce323d56d424e7e59d9d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-21 13:16:31 +0530

    Update Centos8_5.5.3.sh

--- Centos8_5.5.3.sh
+++ Centos8_5.5.3.sh
@@ -3,6 +3,7 @@
 # 5.5.3 Ensure default user shell timeout is 900 seconds or less
 echo
 echo \*\*\*\* 5.5.3 Ensure default user shell timeout is 900 seconds or less
-echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/bashrc
-echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/profile.d/*.sh 
-echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/profile
+#echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/bashrc
+#echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/profile.d/*.sh 
+#echo -e "TMOUT=900 \nreadonly TMOUT \nexport TMOUT" >> /etc/profile
+echo -e "readonly TMOUT=900 ; export TMOUT" >> /etc/profile
