a05d98f8be13d755b007ffe63462a98f04f0cf1d
revanth-96 <87057728+revanth-96@users.noreply.github.com>
2022-03-22 11:06:48 +0530

    Update Centos8_1.1.2.sh

--- Centos8_1.1.2.sh
+++ Centos8_1.1.2.sh
@@ -3,7 +3,10 @@
 # 1.1.2 Ensure /tmp is configured
 echo
 echo \*\*\*\* 1.1.2 Ensure /tmp is configured
-findmnt /tmp || (echo "tmpfs /tmp tmpfs defaults,rw,nosuid,nodev,noexec,relatime 0 0" >>/etc/fstab && mount /tmp)
-
+#findmnt /tmp || (echo "tmpfs /tmp tmpfs defaults,rw,nosuid,nodev,noexec,relatime 0 0" >>/etc/fstab && mount /tmp)
+echo -e "[Mount] \nWhat=tmpfs \nWhere=/tmp \nType=tmpfs \nOptions=mode=1777,strictatime,noexec,nodev,nosuid" >> /etc/systemd/system/tmp.mount
+systemctl daemon-reload
+systemctl unmask tmp.mount
+systemctl --now enable tmp.mount
 
 
